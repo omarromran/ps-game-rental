b7f28745f6820455fa2fa16a24a980eb9f55b6d8
zeinaahmed06 <zenaelkordy@yahoo.com>
2026-03-25 13:08:32 +0200

    rentals changes

--- myRentals.js
+++ myRentals.js
@@ -1,26 +1,18 @@
-function openReviewModal(gameName){
+function openReviewModal(gameName) {
+    const modal = document.getElementById("review-modal");
+    const gameTitle = document.getElementById("review-game-name");
 
-const modal = document.getElementById("review-modal");
-const gameTitle = document.getElementById("review-game-name");
-
-gameTitle.textContent = "Game: " + gameName;
-
-modal.classList.add("active");
-
+    gameTitle.textContent = "Game: " + gameName;
+    modal.style.display = "flex";
 }
 
-function closeReviewModal(){
-
-document.getElementById("review-modal").classList.remove("active");
-
+function closeReviewModal() {
+    document.getElementById("review-modal").style.display = "none";
 }
 
-window.onclick = function(event){
-
-const modal = document.getElementById("review-modal");
-
-if(event.target === modal){
-modal.classList.remove("active");
-}
-
+window.onclick = function (event) {
+    const modal = document.getElementById("review-modal");
+    if (event.target === modal) {
+        modal.style.display = "none";
+    }
 }
