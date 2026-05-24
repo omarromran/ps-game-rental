3d3b6b7cbac44bc5e31558935ec3997715bb4b93
zeinaahmed06 <zenaelkordy@yahoo.com>
2026-05-24 15:49:33 +0300

    authController changed

--- BackEnd/Routes/authRoutes.js
+++ BackEnd/Routes/authRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { register, login, logout, getMe } = require('../Controllers/authControllers');
+const { register, login, logout, getMe } = require('../Controllers/authController');
 
 router.post('/register', register);
 router.post('/login', login);
