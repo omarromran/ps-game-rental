460cbc1be8e2d1b25fc3199729b8ece03e5b73fd
AhmedH90 <ahmedhicham957@gmail.com>
2026-05-31 19:02:06 +0300

    dupp

--- BackEnd/Routes/authRoutes.js
+++ BackEnd/Routes/authRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { register, login, logout, getMe } = require('../Controllers/authController');
+const { protect } = require('../Middleware/authMiddleware');
 
 router.post('/register', register);
 router.post('/login', login);
